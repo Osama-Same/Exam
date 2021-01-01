5c1ff85a2e251043f696941332939904af4adcb1
your_github_ <your_email@example.com>
2021-01-01 22:50:15 +0200

    part 3

--- part_3/part_3.js
+++ part_3/part_3.js
@@ -5,25 +5,28 @@ PART 3 [General Information]:
 Type the correct answer below each question:
 
 Q1) Why do we use joins in SQL?
-
+Returns records that have matching values in both tables
 
 Q2) Name three types of relationships in databases.
-
+one and one 
+many and many
+many and one
 
 Q3) What are the main three sections of a JWT?
-
+call back, promses ,async and 
 
 Q4) Name three types of express middle-wares.
-
+  res ,req ,next
 
 Q5) What is the `effect` hook used for in react?
-
+ 
+Building your own Hooks lets you extract component logic into reusable functions.
 
 Q6) What is the `state` hook used for in react?
-
+   بجيب data
 
 Q7) List three thing you can use to create a responsive web application.
-
+Keys,map()
 
 Q8) Name two of mongoose middle-wares and explain what they are used for
 
